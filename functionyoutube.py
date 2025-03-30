4432cab136f79a33dfd5c294efcaad9c47287e3b
brensangura <sangura.bren>
2025-03-30 17:56:40 +0300

    youtue functions

---
+++ functionyoutube.py
@@ -0,0 +1,20 @@
+print("Happy birthday to you")
+print("Happy birthday to you")
+print("Happy birthday dear [Name]")
+print("Happy birthday to you")
+print("Happy birthday to you")
+print("Happy birthday to you")
+
+def birthday_song(name):
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print("Happy birthday dear " + name)
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print("Happy birthday to you")
+    print("Happy birthday dear " + name)
+    print("Happy birthday to you")
+
+
+
+birthday_song("Dr Sangura")
